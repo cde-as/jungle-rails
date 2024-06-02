19df1d5a6dedaf57dc96c7b2a77025f2ec8bed95
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-06-02 13:52:55 -0700

    implement admin security for products page replicate from dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
- 
+  before_action :authenticate
 
   def show
     @product_count = Product.count
@@ -8,5 +8,9 @@ class Admin::DashboardController < ApplicationController
 
   private
 
-
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
 end
